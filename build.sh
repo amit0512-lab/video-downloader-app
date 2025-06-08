0569cb7d1801ac557cdca527e49c613b51a1279b
amit0512-lab <knpknp599@gmail.com>
2025-06-08 10:00:04 +0530

    Add build script to install yt-dlp and instaloader

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-
-# Update system packages
 apt-get update
-
-# Install Python and pip
 apt-get install -y curl python3-pip
-
-# Install yt-dlp and instaloader
 pip3 install -U yt-dlp
 pip3 install instaloader
